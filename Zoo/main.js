08cb61c06c0d59fe65257883e9117fef6b5227bd
mikyYun <mkyun2714@gmail.com>
2022-10-07 19:58:52 -0700

    feat set foodLevel

--- Zoo/main.js
+++ Zoo/main.js
@@ -1,24 +1,25 @@
 class Animal {
-  constructor(name, type, foodType, swim) {
+  constructor(name, type, foodType, swim, species) {
     this.name = name;
     this.type = type;
     this.foodType = foodType;
     this.swimmable = swim;
-    this.foodLevel = Math.floor(Math.random() * 100)
+    this.species = species
+    this.foodLevel = Math.floor(Math.random() * 100);
     this.init();
 
   }
   init() {
     console.log(`${this.name} in ${this.type} added and it's foodType is ${this.foodType}`, `FoodLevel: ${this.foodLevel}`);
   }
-  
+
 }
 class Mammal extends Animal {
   // constructor(name, type, foodType, swim) {
   //   super(name, type, foodType, swim);
   // }
 
-  
+
 }
 
 class Bird extends Animal {
@@ -49,6 +50,8 @@ const generateButton = document.querySelector(".button_generate");
 const addSelector = document.querySelector(".animal_lists");
 const animalName = document.querySelector(".new_animal_name");
 
+const animalContainer = document.querySelector(".animals_container");
+
 /** TYPES */
 const types = {
   Mammal: ["Cat", "Lion", "Fox", "Rabbit", "Sheep"],
@@ -56,10 +59,10 @@ const types = {
   Raptile: ["Dino"]
 };
 const foodTypes = {
-  Omnivore : ["Cat", "Duck"],
-  Herbivore : ["Chicken", "Rabbit", "Sheep"],
-  Carnivore : ["Dino", "Fox", "Lion", "Penguin"]
-}
+  Omnivore: ["Cat", "Duck"],
+  Herbivore: ["Chicken", "Rabbit", "Sheep"],
+  Carnivore: ["Dino", "Fox", "Lion", "Penguin"]
+};
 const swimmable = ["cat", "Lion", "Fox", "Penguin", "Duck"];
 
 
@@ -87,17 +90,53 @@ function generateClass(name, type, foodType) {
   const canSwim = swimmable.includes(type) ? "Yes" : "No";
 
   let newAnimal;
-  if (types.Mammal.includes(type)) newAnimal = new Mammal(name, type, foodType, canSwim);
-  if (types.Bird.includes(type)) newAnimal = new Bird(name, type, foodType, canSwim);
-  if (types.Raptile.includes(type)) newAnimal = new Raptile(name, type, foodType, canSwim);
+  if (types.Mammal.includes(type)) newAnimal = new Mammal(name, type, foodType, canSwim, "Mammal");
+  if (types.Bird.includes(type)) newAnimal = new Bird(name, type, foodType, canSwim, "Bird");
+  if (types.Raptile.includes(type)) newAnimal = new Raptile(name, type, foodType, canSwim, "Raptile");
   return newAnimal;
 }
 
 /** CREATE HTML ELEMENTS */
 function createElement(animal) {
   // distructuring
-  const {name, type, swimmable, foodType, foodLevel} = animal;
-  console.log("HEY", name, type, swimmable, foodType, foodLevel)
+  const { name, type, swimmable, foodType, foodLevel, species } = animal;
+  console.log("HEY", name, type, swimmable, foodType, foodLevel, species);
+  // parent = class("animals_container")
+  /** CHILD */
+  const child = document.createElement("div");
+  child.setAttribute("class", name);
+  child.innerHTML = `
+  <div class="animal ${type}">
+    <img src="./images/animals/${type}.png" alt=${type} class="animal_img">
+    <div class="animal_detail">
+      <div class="animal_species">
+        ${species}
+      </div>
+      <div class="animal_name_container">
+        <span class="animal_name_title">
+          Name:
+        </span>
+        ${name}
+      </div>
+      <div class="animal_character_container">
+        <div class="animal_character">
+          <span class="animal_character_title">Character:</span>
+          good
+        </div>
+      </div>
+      <div for="animal_foodLevel_container">
+        <span class="animal_foodLevel_title">
+          FoodLevel
+        </span>
+        <div class="animal_foodLevel">
+          <div class="gauge" style="width: ${foodLevel}%">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  animalContainer.appendChild(child);
 }
 
 /** Create Animal Component */
@@ -109,7 +148,7 @@ function generateAnimal(e) {
   if (foodType) {
     let newAnimal = generateClass(name, type, foodType);
     /** APPEND CHILD */
-    createElement(newAnimal)
+    createElement(newAnimal);
   }
 
 }
