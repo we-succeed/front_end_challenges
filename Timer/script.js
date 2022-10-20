badac456f83a25fdb922caa6fd84f8227a8d6ef4
HANA-BOOK\smile <smilecana@gmail.com>
2022-10-19 22:43:23 -0700

    Feature: Added the stopwatch

---
+++ Timer/script.js
@@ -0,0 +1,36 @@
+const watch = document.querySelector('#watch');
+let milliseconds = 0;
+let timer;
+
+function startWatch() {
+  watch.classList.remove('paused');
+  clearInterval(timer);
+  timer = setInterval(()=>{ 
+    milliseconds += 10;
+    let dateTimer = new Date(milliseconds);
+    watch.innerHTML = 
+      ('0'+dateTimer.getUTCHours()).slice(-2) + ':' +
+      ('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
+      ('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
+      ('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
+  },10);
+};
+
+function pauseWatch() {
+  watch.classList.add('paused');
+  clearInterval(timer);
+};
+
+function resetWatch() {
+  watch.classList.remove('paused');
+  clearInterval(timer);
+  milliseconds = 0;
+  watch.innerHTML= '00:00:00:00';
+};
+
+document.addEventListener('click', (e) =>{
+  const el = e.target;
+  if (el.id === 'start') startWatch();
+  if (el.id === 'pause') pauseWatch();
+  if (el.id === 'reset') resetWatch();
+});
