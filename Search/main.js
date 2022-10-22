3f9d10da43e17372b4cd3fed99a3cf126e6319ef
mikyYun <mkyun2714@gmail.com>
2022-10-22 13:33:53 -0700

    feat v1

---
+++ Search/main.js
@@ -0,0 +1,49 @@
+let canada;
+
+const fetching = () => {
+  fetch("./canada.json")
+    .then(res => res.json())
+    .then(data => {
+      canada = data;
+    })
+    .catch(err => console.error("ERROR", err));
+};
+fetching();
+
+const findMatches = (regex) => {
+  return canada.filter(location => {
+
+    // regex can't accept dynamic value. Use new RegExp
+    
+    return location[0].match(regex) || location[1].match(regex);
+  });
+};
+
+const displayLists = (e) => {
+  if (!e.target.value) return lists.innerHTML = "";
+  const input = e.target.value;
+  const regex = new RegExp(input, "gi");
+  const filteredLocations = findMatches(regex);
+
+  const newLists = filteredLocations.map(location => {
+    const cityName = location[0].replace(regex, `
+    <span class="highlight">${input}</span>
+    `)
+    const stateName = location[1].replace(regex, `
+    <span class="highlight">${input}</span>
+    `)
+    
+    return `
+      <li>
+      <span class="name">${cityName}, ${stateName}</span>
+      </li>
+    `
+
+  }).join("");
+  lists.innerHTML = newLists;
+};
+
+const searchInput = document.querySelector(".search_input");
+const lists = document.querySelector(".lists");
+searchInput.addEventListener("change", displayLists);
+searchInput.addEventListener("keyup", displayLists);
