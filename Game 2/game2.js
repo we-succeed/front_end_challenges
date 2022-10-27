040dbe43d0c03d693530070d30fa8f29e3882c6c
“Kyuhyeon <mayfly950@gmail.com>
2022-10-26 17:39:54 -0700

    test

--- Game 2/game2.js
+++ Game 2/game2.js
@@ -1,23 +1,29 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){return}
+var counter = 0;
+function jump() {
+    if (character.classList == "animate") { return }
     character.classList.add("animate");
-    setTimeout(function(){
+    setTimeout(function () {
         character.classList.remove("animate");
-    },300);
+    }, 300);
 }
-var checkDead = setInterval(function() {
+var checkDead = setInterval(function () {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if (blockLeft < 40 && blockLeft > 0 && characterTop >= 135) {
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
+        alert("Game Over. score: " + Math.floor(counter / 100));
+        counter = 0;
         block.style.animation = "block 1s infinite linear";
-    }else{
+    } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 }, 10);
+
+const refreshButton = document.querySelector('.reset');
+const refreshPage = function () {
+    location.reload();
+};
+refreshButton.addEventListener('click', refreshPage);
